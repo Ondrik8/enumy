2fb760eab825f4f0465c1be3305dd1d45ceeae7b
luke.goddard <l-goddard@protonmail.com>
2020-04-22 10:16:09 +0100

    reads config file for passwords

--- Makefile
+++ Makefile
@@ -27,10 +27,10 @@ $(EXE_SCAN): $(OBJ_SCAN)
 	$(CC) $(LDFLAGS) $^ $(LDLIBS) -o $@ -lncursesw -lpthread $(STATIC) $(ARCH) -g 
 	
 $(OBJ_SCAN_DIR)/%.o: $(SCAN_DIR)/%.c | $(OBJ_SCAN_DIR)
-	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@ -lncursesw -lpthread $(STATIC) $(ARCH) -g
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@ -lncursesw -lpthread $(STATIC) $(ARCH) -g 
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
-	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@ -lncursesw -lpthread $(STATIC) $(ARCH) -g
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@ -lncursesw -lpthread $(STATIC) $(ARCH) -g 
 
 $(OBJ_DIR):
 	mkdir $@
--- src/scans/interesting_files_scan.c
+++ src/scans/interesting_files_scan.c
@@ -5,6 +5,8 @@
     writable config files and coredump files. 
 */
 
+#define _GNU_SOURCE
+
 #include "main.h"
 #include "results.h"
 #include "utils.h"
@@ -12,6 +14,7 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -26,6 +29,8 @@ static int extension_checker(File_Info *fi, All_Results *ar, Args *cmdline);
 static int file_name_checker(File_Info *fi, All_Results *ar, Args *cmdline);
 static bool check_for_encryption_key(File_Info *fi, All_Results *ar, Args *cmdline);
 static double caclulate_file_entropy(char *file_location);
+static int search_conf_for_pass(File_Info *fi, All_Results *ar, Args *cmdline);
+static int search_line(char *line_start, char *line_end);
 
 // Kicks of other scans
 int intresting_files_scan(File_Info *fi, All_Results *ar, Args *cmdline)
@@ -33,7 +38,7 @@ int intresting_files_scan(File_Info *fi, All_Results *ar, Args *cmdline)
     int findings = 0;
 
     findings += extension_checker(fi, ar, cmdline);
-    // findings += file_name_checker(fi, ar, cmdline);
+    findings += file_name_checker(fi, ar, cmdline);
     // findings += parent_dir_checker(fi, ar, cmdline);
 }
 
@@ -49,6 +54,8 @@ static int extension_checker(File_Info *fi, All_Results *ar, Args *cmdline)
             findings = (check_for_encryption_key(fi, ar, cmdline) == true) ? findings++ : findings;
         break;
     case 'c':
+        if (strcmp(fi->extension, "conf") == 0)
+            findings += search_conf_for_pass(fi, ar, cmdline);
         break;
     case 'd':
         if (strcmp(fi->extension, "des") == 0)
@@ -112,6 +119,22 @@ static int extension_checker(File_Info *fi, All_Results *ar, Args *cmdline)
 // Kick of scans that require the file name to classify the file
 static int file_name_checker(File_Info *fi, All_Results *ar, Args *cmdline)
 {
+    int findings = 0;
+    if (strcasestr(fi->name, "id_rsa") != NULL && strcmp(fi->extension, "pub") != 0)
+    {
+        if (check_for_encryption_key(fi, ar, cmdline))
+        {
+            findings++;
+        }
+    }
+    if (strcasestr(fi->name, "id_dsa") != NULL && strcmp(fi->extension, "pub") != 0)
+    {
+        if (check_for_encryption_key(fi, ar, cmdline))
+        {
+            findings++;
+        }
+    }
+    return findings;
 }
 
 // Kick of the scans that require the  parent directory to identify the file
@@ -230,4 +253,121 @@ static double caclulate_file_entropy(char *file_location)
         free(hist);
     }
     return H;
+}
+
+// Maps the entired file into memory
+static int search_conf_for_pass(File_Info *fi, All_Results *ar, Args *cmdline)
+{
+
+    int fd;
+    int findings = 0;
+    unsigned char *f_data, *f_data_end;
+    unsigned char *line_begin, *line_end;
+
+    if (fi->stat->st_size == 0 || (fd = open(fi->location, O_RDONLY) == -1))
+    {
+        return findings;
+    }
+
+    f_data = mmap(0, fi->stat->st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (f_data == MAP_FAILED)
+    {
+        close(fd);
+        return findings;
+    }
+
+    f_data_end = f_data + fi->stat->st_size;
+    line_begin = line_end = f_data;
+
+    while (true)
+    {
+        // Itterate until we find a new line or EOF
+        while ((line_end < f_data_end) && ((*line_end != '\n') || (*line_end != '\r') || (*line_end != 0x0a)))
+        {
+            if (line_end - line_begin >= 3)
+            {
+                if (
+                    ((unsigned char)*line_end == 0xe2) &&
+                    ((unsigned char)*line_end - 1 == 0x90) &&
+                    ((unsigned char)*line_end - 2 == 0xa4))
+                {
+                    break; // utf8 new line
+                }
+            }
+            line_end++;
+        }
+        if (search_line(line_begin, line_end))
+        {
+            int id = 47;
+            char *name = "Config could contain passwords";
+            Result *new_result = create_new_issue();
+            set_id_and_desc(id, new_result);
+            set_issue_location(fi->location, new_result);
+            set_issue_name(name, new_result);
+            add_new_result_info(new_result, ar, cmdline);
+            findings++;
+            break;
+        }
+        if (line_end == f_data_end)
+        {
+            break;
+        }
+        else
+        {
+            line_end = line_end++;
+            line_begin = line_end;
+        }
+    }
+
+    close(fd);
+    munmap((void *)f_data, fi->stat->st_size);
+    return findings;
+}
+
+static int search_line(char *line_start, char *line_end)
+{
+    char *buffer = malloc((2 + (line_end - line_start)) * sizeof(char));
+    int loc = 0;
+    int findings = 0;
+    bool in_whitespace = true;
+    bool first_non_white = false;
+    bool equals_found = false;
+
+    if (buffer == NULL)
+    {
+        return 0;
+    }
+    for (char *i = line_start; i <= line_end; i++)
+    {
+        if (in_whitespace && (*i == ' ' || *i == '\t'))
+        {
+            first_non_white = true;
+        }
+        else
+        {
+            if (first_non_white && *i == '#')
+            {
+                // line is commented
+                free(buffer);
+                return findings;
+            }
+            first_non_white = false;
+            in_whitespace == false;
+            if (*i == '=')
+            {
+                equals_found = true;
+            }
+        }
+        buffer[loc] = *i;
+        loc++;
+    }
+    buffer[loc++] = '\0';
+
+    if (equals_found && strcasestr(buffer, "password") != NULL)
+    {
+        findings++;
+    }
+
+    free(buffer);
+    return findings;
 }
